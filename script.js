7e0e994ad25f4c19651437e217df394f1e60433d
LakshmiNarayanan280406 <planar2006@gmail.com>
2024-03-31 13:32:09 +0530

    Created a function to get player chocie.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 alert("Open the Console");
+// Get Computer Choice
 function getComputerChoice(){
     let compChoice=Math.floor(Math.random()*3)+1;
     if (compChoice===1){
@@ -11,3 +12,23 @@ function getComputerChoice(){
         return 'paper';
     }
 }
+
+// Get Player Chocie
+function getPlayerChoice(){
+    let playChoice=prompt("Rock, Paper or Scissors");
+    playChoice=playChoice.toLowerCase();
+    if (playChoice==='rock'){
+        return 'rock';
+    }
+    else if (playChoice==='scissors' || playChoice==='scissor'){
+        return 'scissors';
+    }
+    else if (playChoice==='paper'){
+        return 'paper';
+    }
+    else {
+        alert("Invalid Choice");
+        getPlayerChoice();
+    }
+}
+
